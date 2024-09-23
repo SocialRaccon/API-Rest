2afec1f8181fbe16edc5a8db62440edf196ac5d1
alex2227 <alex2227@hotmail.com>
2024-09-22 21:33:18 -0600

    First version App

---
+++ src/test/java/itst/social_raccoon_api/SocialRaccoonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package itst.social_raccoon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialRaccoonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
