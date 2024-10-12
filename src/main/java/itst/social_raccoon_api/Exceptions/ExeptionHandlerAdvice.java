73c061f4be62ace92e6fec95d3912bcf9b434982
alex2227 <alex2227@hotmail.com>
2024-10-12 15:23:51 -0600

    Added exeptions for HttpMessageNotReadableException and ConstraintViolationException

--- src/main/java/itst/social_raccoon_api/Exceptions/ExeptionHandlerAdvice.java
+++ src/main/java/itst/social_raccoon_api/Exceptions/ExeptionHandlerAdvice.java
@@ -2,8 +2,11 @@ package itst.social_raccoon_api.Exceptions;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.hibernate.validator.internal.engine.ConstraintViolationImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -71,19 +74,31 @@ public class ExeptionHandlerAdvice {
                 .body("Illegal argument provided: " + e.getMessage());
     }
 
-    // 500 - Internal Server Error for all other exceptions*
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<?> handleGeneralException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("An unexpected error occurred: " + e.getMessage());
-    }
-
-
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body("Entity not found: " + e.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException e) {
+        StringBuilder message = new StringBuilder("Constraint violation: ");
+        e.getConstraintViolations().forEach(violation -> {
+            message.append("Parameter '")
+                    .append(violation.getPropertyPath())
+                    .append("' ")
+                    .append(violation.getMessage())
+                    .append(". ");
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message.toString());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Invalid request body: " + e.getMessage());
     }
 }
