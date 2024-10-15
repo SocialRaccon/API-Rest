01eec8446f1e87e88693f1c70255d845c2285a8a
alex2227 <alex2227@hotmail.com>
2024-10-14 23:28:55 -0600

    Added Rest annotations and doc annotations

--- src/main/java/itst/social_raccoon_api/Controllers/PostController.java
+++ src/main/java/itst/social_raccoon_api/Controllers/PostController.java
@@ -1,4 +1,15 @@
 package itst.social_raccoon_api.Controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("post")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@Tag(name = "Post", description = "Provide methods to manage posts")
 public class PostController {
+
 }
