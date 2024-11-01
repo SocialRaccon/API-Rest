a5b42f92ae1f767d73b3cfbabed6257e43fe5bc7
MarcoAntonioHer <lm6821277@gmail.com>
2024-11-01 10:32:28 -0600

    Test relationshiptest

--- src/test/java/itst/social_raccoon_api/controllers/RelationshipControllerTest.java
+++ src/test/java/itst/social_raccoon_api/controllers/RelationshipControllerTest.java
@@ -1,30 +1,37 @@
 package itst.social_raccoon_api.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import itst.social_raccoon_api.dtos.RelationshipInfoDTO;
+import itst.social_raccoon_api.services.RelationshipService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RelationshipControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @Autowired
-    private ReactionController controller;
+    private RelationshipController controller;
+
+    @Autowired
+    private RelationshipService relationshipService;
 
     @Test
     void contextLoads() throws Exception {
@@ -33,16 +40,18 @@ public class RelationshipControllerTest {
 
     @Test
     public void followUserTest() throws Exception {
-        mvc.perform(post("/relationships/1/3") // Juan follows Pedro
+        mockMvc.perform(post("/relationships/1")
+                        .param("followerId", "2")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("User followed"));
     }
-
+    
     @Test
     public void unfollowUserTest() throws Exception {
-        mvc.perform(delete("/relationships/1/2") // Juan stops following María
+        mockMvc.perform(delete("/relationships/1")
+                        .param("followerId", "2")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -50,20 +59,24 @@ public class RelationshipControllerTest {
     }
 
     @Test
-    public void getFollowersAndFollowingTest() throws Exception {
-        mvc.perform(get("/relationships/1") // Get information from Juan
+    public void getFollowersPaginationTest() throws Exception {
+        mockMvc.perform(get("/relationships/followers/1")
+                        .param("page", "1")
+                        .param("size", "5")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.followers", hasSize(1))) // Juan has 2 followers
-                .andExpect(jsonPath("$.following", hasSize(2))); // Juan follows 2 users
+                .andExpect(jsonPath("$", hasSize(lessThanOrEqualTo(5))));
     }
 
     @Test
-    public void getFollowersAndFollowingNotFoundTest() throws Exception {
-        mvc.perform(get("/relationships/10") // Search for a user that does not exist
+    public void getFollowingPaginationTest() throws Exception {
+        mockMvc.perform(get("/relationships/following/1")
+                        .param("page", "1")
+                        .param("size", "5")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isNotFound());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(lessThanOrEqualTo(5))));
     }
 }
