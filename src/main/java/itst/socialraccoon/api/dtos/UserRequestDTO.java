70f3ac9167b7f4b8d066361320ad76a5e3b8642d
Kintx2227 <alex2227@hotmail.com>
2024-11-08 16:40:04 -0600

    Added exception handling with html

--- src/main/java/itst/socialraccoon/api/dtos/UserRequestDTO.java
+++ src/main/java/itst/socialraccoon/api/dtos/UserRequestDTO.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import org.checkerframework.checker.regex.qual.Regex;
 
 @Schema(description = "Data Transfer Object representing a user request")
 public class UserRequestDTO {
