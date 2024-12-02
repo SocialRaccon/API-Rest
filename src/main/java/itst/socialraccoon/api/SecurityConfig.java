5681df4fc51ffc5e09398d4e7f7e75fed585afcd
kintx2227 <alex2227@hotmail.com>
2024-12-01 19:57:58 -0600

    Activated security for test angular app

--- src/main/java/itst/socialraccoon/api/SecurityConfig.java
+++ src/main/java/itst/socialraccoon/api/SecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -23,16 +26,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         try {
             http
+                    .cors(withDefaults())
                     .csrf(csrf -> csrf.disable()) // Disable CSRF protection (optional, but often needed for API)
                     .authorizeHttpRequests(auth -> auth
                             .requestMatchers("/authentications/recover", "/authentications/change", "/signin", "/sigup").permitAll()
                             .requestMatchers(HttpMethod.GET, "/careers", "/careers/").permitAll()
                             .requestMatchers(HttpMethod.POST, "/users/withImage",  "/users").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/").permitAll()
-                            .requestMatchers(HttpMethod.POST, "/").permitAll()
-                            .requestMatchers(HttpMethod.PUT, "/").permitAll()
-                            .requestMatchers(HttpMethod.DELETE, "/").permitAll()
-                            .anyRequest().permitAll())
+                            .requestMatchers(HttpMethod.GET, "/").authenticated()
+                            .requestMatchers(HttpMethod.POST, "/").authenticated()
+                            .requestMatchers(HttpMethod.PUT, "/").authenticated()
+                            .requestMatchers(HttpMethod.DELETE, "/").authenticated()
+                            .anyRequest().authenticated())
                     .httpBasic(Customizer.withDefaults()) // Use HTTP Basic authentication
                     .formLogin(withDefaults())
                     .rememberMe(withDefaults())
@@ -55,6 +59,19 @@ public class SecurityConfig {
                     .password(authentication.getPassword())
                     .build();
         };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200"); // Reemplaza con la URL de tu frontend
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
